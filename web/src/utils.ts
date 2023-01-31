5593a5e94d5f92b1f7b8895bedad4bdd21bec4b0
Bayu Setiawan <bayusetiawan37@gmail.com>
2023-01-31 21:59:13 +0700

    feat: add error response

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -6,49 +6,57 @@ export const fetchApi = async <T>(
   url: string,
   body: any | null,
   token?: string
-): Promise<IResponse<T> | null> => {
-  const headers: { [key: string]: string } = {
-    Accept: "application/json",
-  };
-  const reqInit: RequestInit = { method: method };
-  if (body) {
-    reqInit.body = JSON.stringify(body);
-    headers["Content-Type"] = "application/json";
-  }
-  if (token) {
-    headers["Authorization"] = token;
-  }
-  reqInit.headers = headers;
-  const resp = await fetch(url, reqInit);
-  return await resp.json();
-};
-
-export const useFetch = <T = any>(method: string, url: string) => {
-  const isLoading = ref(false);
-  const data = ref<T | null>(null);
-  const error = ref<IErrorState | null>(null);
-  const execute = async (body?: any, token?: string) => {
-    try {
-      isLoading.value = true;
-      data.value = null;
-      error.value = null;
-      const resp = await fetchApi<T>(method, url, body, token);
-
-      if (resp && resp.code == 0) {
-        data.value = resp.data as any;
-      } else {
-        // todo get response message
-        throw new Error("Unexpected response body");
-      }
-    } catch (err) {
-      error.value = {
-        message: `Error "${err}" when fetching ${method} ${url}`,
-      };
-      data.value = null;
-    } finally {
-      isLoading.value = false;
+  ): Promise<IResponse<T> | null> => {
+    const headers: { [key: string]: string } = {
+      Accept: "application/json",
+    };
+    const reqInit: RequestInit = { method: method };
+    if (body) {
+      reqInit.body = JSON.stringify(body);
+      headers["Content-Type"] = "application/json";
     }
+    if (token) {
+      headers["Authorization"] = token;
+    }
+    reqInit.headers = headers;
+    const resp = await fetch(url, reqInit);
+    return await resp.json();
   };
-
-  return { isLoading, data, error, execute };
-};
+  
+  export const useFetch = <T = any>(method: string, url: string) => {
+    const isLoading = ref(false);
+    const data = ref<T | null>(null);
+    const error = ref<IErrorState | null>(null);
+    const execute = async (body?: any, token?: string) => {
+      try {
+        isLoading.value = true;
+        data.value = null;
+        error.value = null;
+        const resp = await fetchApi<T>(method, url, body, token);
+        
+        if (resp && resp.code == 0) {
+          data.value = resp.data as any;
+        } else if (resp && resp.code != undefined) {
+          throw {...resp}
+        } else {
+          throw new Error("Unexpected response body");
+        }
+      } catch (err) {
+        if ((err as any).message != undefined) {
+          error.value = {
+            message: (err as any).message,
+          };
+        } else {
+          error.value = {
+            message: `Error "${err}" when fetching ${method} ${url}`,
+          };
+        }
+        data.value = null;
+      } finally {
+        isLoading.value = false;
+      }
+    };
+    
+    return { isLoading, data, error, execute };
+  };
+  
