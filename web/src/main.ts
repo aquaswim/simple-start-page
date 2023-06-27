a97d470e477146047f41abd1109b504067ac0dcf
Bayu Setiawan <bayusetiawan37@gmail.com>
2023-06-27 17:33:13 +0700

    chore: bundle material icon and add link to material icon reference

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "material-icons/iconfont/material-icons.css"
 import "@picocss/pico"
 import "./assets/main.css";
 
